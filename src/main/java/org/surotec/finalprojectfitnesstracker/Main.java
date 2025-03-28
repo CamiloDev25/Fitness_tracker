31dd86ef2babc95c9e61def0c2c06247cf3e8392
Camilo Rios Olaya <camilo.rios@globant.com>
2025-03-28 18:23:09 -0500

    The basic structure of the project packages was created and the  main class was created to advance in the options menu

---
+++ src/main/java/org/surotec/finalprojectfitnesstracker/Main.java
@@ -0,0 +1,19 @@
+package org.surotec.finalprojectfitnesstracker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Your Personal Fitness Tracker \n" +
+                "\n Please select an option: " +
+                "\n\n 1. Show Workouts " +
+                "\n- View the list of available workouts and see detailed structures to help" +
+                "\n\n 2. Log Workout " +
+                "\n- Select a workout you have performed and enter the time taken for each exercise" +
+                "\n\n 3. View Logged Workouts " +
+                "\n- Review your workout history, including details and total time for each exercise");
+
+    }
+}
