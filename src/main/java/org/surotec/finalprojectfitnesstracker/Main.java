78348bb029e9d184e8ccd256210b2f71302ea338
Camilo Rios Olaya <camilo.rios@globant.com>
2025-04-02 13:07:30 -0500

    New methods were developed to make the code more SOLID. The programa was designed with the ability to go back and forward in the menu

--- src/main/java/org/surotec/finalprojectfitnesstracker/Main.java
+++ src/main/java/org/surotec/finalprojectfitnesstracker/Main.java
@@ -1,6 +1,5 @@
 package org.surotec.finalprojectfitnesstracker;
 
-import org.surotec.finalprojectfitnesstracker.application.ExerciseController;
 import org.surotec.finalprojectfitnesstracker.application.WorkoutController;
 import org.surotec.finalprojectfitnesstracker.domain.dto.Exercise;
 import org.surotec.finalprojectfitnesstracker.domain.dto.Workout;
@@ -8,13 +7,69 @@ import org.surotec.finalprojectfitnesstracker.domain.dto.Workout;
 import javax.sql.rowset.serial.SerialException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-        String selectedOption;
+        getMainMenu(input);
+    }
+
+    private static void showWorkout(Scanner input) {
+        WorkoutController workoutController = new WorkoutController();
+        List<Workout> workoutList = workoutController.findAll();
+        System.out.println("The available Workouts: ");
+        int i = 1;
+        for (Workout workout : workoutList) {
+            System.out.println(i + ". " + workout.getTitle() + " " + workout.getDescription());
+            i++;
+        }
+
+        //while (true){
+        System.out.println("Please select a workout by entering its number (1-" + (i -1) + "): or if you regreat write back ");
+        String  userInput = input.nextLine();
+        int workoutChoice = 0;
+        try {
+            workoutChoice = Integer.parseInt(userInput);
+
+        }catch (NumberFormatException woC){
+                returnToMainMenu( input, userInput); //----------
+        }
+
+        // get workout selected
+        showWorkoutExercises(input, workoutChoice, i, workoutList);
+    }
+
+    private static void showWorkoutExercises(Scanner input, int workoutChoice, int i, List<Workout> workoutList) {
+        if (workoutChoice >= 1 && workoutChoice < i) {
+            Workout selectedWorkout = workoutList.get(workoutChoice - 1);
+
+            // workout details
+            System.out.println("\nWorkout Structure: " + selectedWorkout.getTitle());
+            System.out.println("Description: " + selectedWorkout.getDescription());
+            System.out.println("Exercises:");
+
+            // show workout exercises
+            for (Exercise exercise : selectedWorkout.getExercises()) {
+                System.out.println(" - " + exercise.getTitle() + ": " + exercise.getDescription());
+            }
+
+            System.out.println("\nNotes: Ensure proper form and take 1-minute rest between sets.");
+            System.out.println("Press any key to return to the workout list...");
+             String message = input.nextLine();
+            if ( isUserMovingBack(message)){
+                showWorkout(input);
+            }else {
+                System.out.println("Option is not value");
+            }
+
+        } else {
+            System.out.println("Invalid choice, please select a number between 1 and " + (i - 1) + ".");
+        }
+    }
+
+    private static void getMainMenu(Scanner input) {
+
         System.out.println("Welcome to Your Personal Fitness Tracker \n" +
                 "\n Please select an option: " +
                 "\n\n 1. Show Workouts " +
@@ -22,66 +77,44 @@ public class Main {
                 "\n\n 2. Log Workout " +
                 "\n- Select a workout you have performed and enter the time taken for each exercise" +
                 "\n\n 3. View Logged Workouts " +
-                "\n- Review your workout history, including details and total time for each exercise");
+                "\n- Review your workout history, including details and total time for each exercise" +
+                "\n\n 4. Exit");
 
         try{
-       selectedOption = input.nextLine();
-        switch (selectedOption) {
-            case "1":
-                WorkoutController workoutController = new WorkoutController();
-                List<Workout> workoutList = workoutController.findAll();
-                System.out.println("The available Workouts: ");
-                int i = 1;
-                for (Workout workout : workoutList) {
-                    System.out.println(i + ". " + workout.getTitle() + " " + workout.getDescription());
-                    i++;
-                }
+           String selectedOption = input.nextLine();
+            switch (selectedOption) {
+                case "1":
+                    showWorkout(input);
 
-                int workoutChoice;
-                while (true){
-                    System.out.println("Please select a workout by entering its number (1-" + (i - 1) + "): ");
-                    workoutChoice = input.nextInt();
-                input.nextLine();
+                case "2":
+                    System.out.println("Log workout selected");
+                    break;
 
-                // get workout selected
-                if (workoutChoice >= 1 && workoutChoice < i) {
-                    Workout selectedWorkout = workoutList.get(workoutChoice - 1);
+                case "3":
+                    System.out.println("View logged workouts selected");
+                    break;
 
-                    // workout details
-                    System.out.println("\nWorkout Structure: " + selectedWorkout.getTitle());
-                    System.out.println("Description: " + selectedWorkout.getDescription());
-                    System.out.println("Exercises:");
+                case "4":
+                    System.out.println("Exiting the program. Goodbye");
+                    input.close();
+                    return;
 
-                    // show workout exercises
-                    for (Exercise exercise : selectedWorkout.getExercises()) {
-                        System.out.println(" - " + exercise.getTitle() + ": " + exercise.getDescription());
-                    }
-
-                    System.out.println("\nNotes: Ensure proper form and take 1-minute rest between sets.");
-                    System.out.println("Press any key to return to the workout list...");
-                    input.nextLine();
-                } else {
-                    System.out.println("Invalid choice, please select a number between 1 and " + (i - 1) + ".");
-                }
-        }
-                
-               /* selectedOption = input.nextLine();
-
-                workoutList.get(1);*/
-                        // the main switch continues
-
-            case "2":
-                break;
-
-            case "3":
-            break;
-
-            default:
-                throw new SerialException("The option is not available");
-        }
+                default:
+                    throw new SerialException("The option is not available");
+            }
         } catch (SerialException ex){
             System.out.println(ex.getMessage()); //message from the throw
         };
-        
+    }
+    private static void returnToMainMenu(Scanner input, String text){
+        if (isUserMovingBack(text)){
+            getMainMenu(input);
+        }else {
+            System.out.println("Please choose other option, This is not value ");
+        }
+    }
+    private static boolean isUserMovingBack(String text){
+        return  text.equals("back");
+
     }
 }
